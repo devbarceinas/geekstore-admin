b57ad80adcdac4992cb6ae6226fa1689e03e8492
Mario <mario@Mac-mini-de-Mac.local>
2022-01-31 15:52:55 -0600

    fix(hooks): resolve issue of useeffect infinite

--- src/hooks/useGetRequest.js
+++ src/hooks/useGetRequest.js
@@ -3,19 +3,22 @@ import { url } from "../config/configAxios";
 
 const useGetRequest = (intialState) => {
   const [products, setProducts] = useState(intialState);
+  const [loading, setLoading] = useState(true);
 
   const allProducts = async () => {
     await url.get('/products')
       .then(response => {
-        setProducts(response.data)
+        setProducts(response.data);
       })
   }
 
-  console.log(products);
-
   useEffect(() => {
-    allProducts();
-  }, []);
+    setLoading(true);
+    if (loading) {
+      allProducts();
+    }
+    return () => setLoading(false);
+  }, [products]);
 
   return products;
 }
